dabb1ee364afbbd5dc8268e9f53a25bc58cd9dd7
TiborKopca <tibor.kopca@gmail.com>
2024-03-26 13:40:39 +0100

    installed react-icons

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 // import React from 'react'
 import { useState } from "react";
+import {FaMapMarker} from 'react-icons/fa'
 
 const JobListing = ({ job }) => {
   //state management for the read more button in the card
@@ -30,7 +31,8 @@ const JobListing = ({ job }) => {
 
         <div className="flex flex-col lg:flex-row justify-between mb-4">
           <div className="text-orange-700 mb-3">
-            <i className="fa-solid fa-location-dot text-lg"></i>
+            {/* <i className="fa-solid fa-location-dot text-lg"></i> */}
+            <FaMapMarker className="inline text-lg mb-1"></FaMapMarker>
             {job.location}
           </div>
           <a
